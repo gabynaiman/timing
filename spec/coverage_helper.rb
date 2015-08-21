d7d50a65f2f58ccca093b38e867216aea25e6523
Gabriel Naiman <gnaiman@keepcon.com>
2015-08-21 17:30:32 -0300

    Ignore autogenerated parser from coverage

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -5,4 +5,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
-SimpleCov.start
+
+SimpleCov.start do
+  add_filter 'natural_time_language.rb'
+end

44944847678390e9c4f0d149418960f5fae93672
Damian Aberbuj <daberbuj@keepcon.com>
2015-08-21 08:41:23 -0500

    Added helpers

--- lib/timing/helpers.rb
+++ lib/timing/helpers.rb
@@ -4,12 +4,37 @@ module Timing
     TIME_REGEXP = /(\d\d:\d\d:\d\d)/
     TIME_FORMAT = '%F %T %z'
 
+    MONTH_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
     def beginning_of_day(time)
       TimeInZone.parse time.strftime(TIME_FORMAT).sub(TIME_REGEXP, '00:00:00')
     end
 
     def end_of_day(time)
       TimeInZone.parse time.strftime(TIME_FORMAT).sub(TIME_REGEXP, '23:59:59')
+    end
+
+    def beginning_of_month(time)
+      TimeInZone.parse time.strftime '%Y-%m-01 00:00:00 %z'
+    end
+
+    def end_of_month(time)
+      day = days_in_month(time.year, time.month)
+      TimeInZone.parse time.strftime "%Y-%m-#{day} 23:59:59 %z"
+    end
+
+    def beginning_of_year(time)
+      TimeInZone.parse time.strftime '%Y-01-01 00:00:00 %z'
+    end
+
+    def end_of_year(time)
+      TimeInZone.parse time.strftime '%Y-12-31 23:59:59 %z'
+    end
+
+    def days_in_month(year, month)
+      return 29 if month == 2 && Date.leap?(year)
+
+      MONTH_DAYS[month-1]
     end
 
   end
--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -2,14 +2,34 @@ require 'minitest_helper'
 
 describe Helpers do
 
-  let(:time) { Time.parse '2015-08-10 02:11:38' }
+  let(:time) { TimeInZone.parse '2015-08-10 02:11:38 +0700' }
   
   it 'Beginning of day' do
-    Timing.beginning_of_day(time).to_time.must_equal Time.parse('2015-08-10 00:00:00')
+    Timing.beginning_of_day(time).to_time.must_equal Time.parse('2015-08-10 00:00:00 +0700')
   end
 
   it 'End of day' do
-    Timing.end_of_day(time).to_time.must_equal Time.parse('2015-08-10 23:59:59')
+    Timing.end_of_day(time).to_time.must_equal Time.parse('2015-08-10 23:59:59 +0700')
+  end
+
+  it 'Beginning of month' do
+    Timing.beginning_of_month(time).to_time.must_equal Time.parse('2015-08-01 00:00:00 +0700')
+  end
+
+  it 'End of month' do
+    Timing.end_of_month(time).to_time.must_equal Time.parse('2015-08-31 23:59:59 +0700')
+  end
+
+  it 'Beginning of year' do
+    Timing.beginning_of_year(time).to_time.must_equal Time.parse('2015-01-01 00:00:00 +0700')
+  end
+
+  it 'End of year' do
+    Timing.end_of_year(time).to_time.must_equal Time.parse('2015-12-31 23:59:59 +0700')
+  end
+
+  it 'Days in month' do
+    Timing.days_in_month(2012, 02).must_equal 29
   end
 
 end

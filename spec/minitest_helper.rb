f58da396ff9c22370f14de70eeb71cddd684f293
Gabriel Naiman <gnaiman@keepcon.com>
2018-12-08 13:07:23 -0300

    Added describe track of minitest line

--- spec/minitest_helper.rb
+++ spec/minitest_helper.rb
@@ -1,7 +1,7 @@
 require 'coverage_helper'
 require 'minitest/autorun'
-require 'minitest/autorun'
 require 'minitest/colorin'
+require 'minitest/line/describe_track'
 require 'pry-nav'
 require 'timing'
 
